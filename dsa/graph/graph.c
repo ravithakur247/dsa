ce1d88cbcf7272db1b9b3ecdaa82f7a7625e3b53
Ravi Thakur <ravithakur@Ravis-MacBook-Air.local>
2026-01-10 15:20:54 +0530

    graph bfs and dfs

--- dsa/graph/graph.c
+++ dsa/graph/graph.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+typedef struct Node
 {
     int vertex;
-    Node *next;
+    struct Node *next;
 
 } Node;
 
-typedef struct
+typedef struct Graph
 {
     int vertices;
     Node **adjlist;
@@ -30,7 +30,7 @@ Graph *create_graph(int vertices)
 
     new_graph->adjlist = malloc(vertices * sizeof(Node *));
 
-    for (size_t i = 0; i < vertices; ++i)
+    for (int i = 0; i < vertices; ++i)
     {
         new_graph->adjlist[i] = NULL;
     }
@@ -44,14 +44,14 @@ void add_edge(Graph *graph, int src, int dest)
     graph->adjlist[src] = dest_node;
 
     // unidirected
-    Node *src_node = create_node(src);
-    src_node->next = graph->adjlist[src];
-    graph->adjlist[src] = src_node;
+    // Node *src_node = create_node(src);
+    // src_node->next = graph->adjlist[src];
+    // graph->adjlist[src] = src_node;
 }
 
 void print_graph(Graph *graph)
 {
-    for (size_t vertex = 0; vertex < graph->vertices; ++vertex)
+    for (int vertex = 0; vertex < graph->vertices; ++vertex)
     {
         printf("vertex %d", vertex);
 
@@ -62,6 +62,7 @@ void print_graph(Graph *graph)
             printf("-> %d", node->vertex);
             node = node->next;
         }
+        printf("\n");
     }
 }
 
@@ -70,7 +71,7 @@ void free_graph(Graph *graph)
     if (!graph)
         return;
 
-    for (size_t vertex = 0; vertex < graph->vertices; ++vertex)
+    for (int vertex = 0; vertex < graph->vertices; ++vertex)
     {
         Node *node = graph->adjlist[vertex];
 
@@ -85,12 +86,113 @@ void free_graph(Graph *graph)
     free(graph);
 }
 
+static void dfs_helper(int src, Graph *graph, int *visited)
+{
+    visited[src] = 1;
+    printf("%d-----", src);
+    Node *temp = graph->adjlist[src];
+    while (temp)
+    {
+        if (!visited[temp->vertex])
+        {
+            dfs_helper(temp->vertex, graph, visited);
+        }
+        temp = temp->next;
+    }
+}
+
+void dfs(Graph *graph)
+{
+    int *visited = calloc(graph->vertices, sizeof(*visited));
+    int components = 0;
+    for (int i = 0; i < graph->vertices; ++i)
+    {
+        if (!visited[i])
+        {
+            dfs_helper(i, graph, visited);
+            components++;
+        }
+    }
+
+    printf("\ncomponents in graphs %d \n", components);
+    free(visited);
+}
+
+void bfs(Graph *graph)
+{
+    int f = 0, r = 0;
+    int *visited = calloc(graph->vertices, sizeof(*visited));
+    int *queue = malloc(graph->vertices * sizeof(*queue));
+
+    queue[r++] = 0;
+    visited[0] = 1;
+
+    while (f < r)
+    {
+        int curr = queue[f++];
+        printf("%d----", curr);
+        Node *temp = graph->adjlist[curr];
+        while (temp)
+        {
+            int v = temp->vertex;
+            if (!visited[v])
+            {
+                visited[v] = 1;
+                queue[r++] = v;
+            }
+            temp = temp->next;
+        }
+    }
+    free(queue);
+    free(visited);
+}
+
 int main()
 {
-    Graph *g = create_graph(5);
+    Graph *g = create_graph(9);
+    // Node 0 has neighbors 1 and 2
     add_edge(g, 0, 1);
-    add_edge(g, 2, 4);
-    add_edge(g, 3, 2);
+    add_edge(g, 0, 2);
+
+    // Other connections
+    add_edge(g, 1, 3);
+    add_edge(g, 1, 4);
+
+    add_edge(g, 2, 5);
+    add_edge(g, 2, 6);
+
+    add_edge(g, 3, 7);
+    add_edge(g, 4, 7);
+
+    add_edge(g, 5, 8);
+    add_edge(g, 6, 8);
+
+    add_edge(g, 7, 9);
+    add_edge(g, 8, 9);
+
     print_graph(g);
+    // dfs(g);
+    bfs(g);
     free_graph(g);
+
+    ///////
+    // Graph *g1 = create_graph(9);
+    // add_edge(g1, 0, 1);
+    // add_edge(g1, 0, 2);
+    // add_edge(g1, 1, 3);
+    // add_edge(g1, 1, 4);
+
+    // // Component 2: Cycle
+    // add_edge(g1, 5, 6);
+    // add_edge(g1, 6, 7);
+    // add_edge(g1, 7, 5);
+
+    // // Component 3: Single chain (just one edge)
+    // add_edge(g1, 8, 8); // or leave 8 disconnected if self-loops not allowed
+
+    // // Component 4: Isolated node
+    // // Node 9 has no edges
+    // print_graph(g1);
+    // dfs(g1);
+    // free_graph(g1);
 }
